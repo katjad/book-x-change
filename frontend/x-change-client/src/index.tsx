2504cb67f433e8e21c72ca5715b659ac1c862b0e
morgap2 <p_lfc_morgan@hotmail.com>
2019-06-12 17:19:36 +0100

    missing semi

--- frontend/x-change-client/src/index.tsx
+++ frontend/x-change-client/src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
