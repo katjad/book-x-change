59c02dfd1c18c220ddd080d51b3f9f8772821e4b
morgap2 <p_lfc_morgan@hotmail.com>
2019-06-12 17:19:57 +0100

    instruction to run storybook demo

--- frontend/x-change-client/src/App.tsx
+++ frontend/x-change-client/src/App.tsx
@@ -3,16 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          x-change-client
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    <strong>x-change-client</strong>
+                </p>
+                <p>
+                    Run 'yarn storybook' to see a demo for semantic-ui-react Button
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
