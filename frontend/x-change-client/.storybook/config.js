6cda119c671ec8a794777e80d2ea46916d952868
morgap2 <p_lfc_morgan@hotmail.com>
2019-06-12 17:18:43 +0100

    storybook TS config

--- frontend/x-change-client/.storybook/config.js
+++ frontend/x-change-client/.storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from '@storybook/react';
 
+const req = require.context('../src/stories', true, /\.stories\.tsx$/);
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
